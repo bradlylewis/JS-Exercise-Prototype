756b789fede393e81e5f2490f542e5cb2cd55dec
Bradly Lewis <bradlylewisdev@gmail.com>
2021-06-16 13:59:13 -0400

    update index.js task 1

--- index.js
+++ index.js
@@ -39,12 +39,40 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
-  }
- 
- 
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+}
 
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible)
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+// const romy = new Person('Romy', 29);
+// const bradly = new Person('Bradly', 27);
+// const jordan = new Person('Jordan', 24);
+
+// console.log(romy);
+// console.log(bradly);
+// console.log(jordan);
+
+// jordan.eat(jordan.eat('taco');
+// jordan.eat(jordan.eat('sandwich');
+// jordan.eat(jordan.eat('pizza');
+// jordan.eat(jordan.eat('noodles');
+// jordan.eat(jordan.eat('bento box');
+// console.log(jordan.stomach);
   
   
   
